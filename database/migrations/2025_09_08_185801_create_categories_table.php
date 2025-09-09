26de52787db789f97d3488154ff38db2dbedc19b
OmarKassem <omar.a.kassem@gmail.com>
2025-09-09 22:16:44 +0300

    Category Management

--- database/migrations/2025_09_08_185801_create_categories_table.php
+++ database/migrations/2025_09_08_185801_create_categories_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('slug')->unique();
+            $table->foreignId('parent_id')->nullable()->constrained('categories')->cascadeOnDelete();
             $table->text('description')->nullable();
             $table->timestamps();
         });
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Category\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -13,3 +14,14 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::post('/verifyOTP', [AuthController::class, 'verifyOTP']);
+
+// ------- Category --------------- //
+
+Route::controller(CategoryController::class)->group(function(){
+    Route::get('index', 'index');
+    Route::get('show/{catID}', 'show');
+    Route::post('store', 'store');
+    Route::put('update/{catID}', 'update');
+    Route::delete('destroy/{catID}', 'destroy');
+});
+
