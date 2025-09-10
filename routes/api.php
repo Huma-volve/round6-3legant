0fcddd86084ed61017db840527b59a37c748f8fd
Ahmed-abdel-halim <ahmedholim42@gmail.com>
2025-09-10 16:45:12 +0300

    solution confict

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,10 @@
 use App\Http\Controllers\Api\V1\Admin\Category\CategoryController;
 use App\Http\Controllers\Api\V1\Admin\Product\ProductController as  AdminProductController;
 use App\Http\Controllers\Api\V1\User\UserController;
-use App\Http\Controllers\Api\admin\Category\CategoryController;
-use App\Http\Controllers\Api\admin\Product\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Api\user\products\ProductController as UserProductController;
+use App\Http\Controllers\Api\V1\User\products\ProductController as UserProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
