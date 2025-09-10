5b830955f4326c1e995d3b06e19d7edce8b53f57
Nayera Sadeek <167679379+NayeraSadeek@users.noreply.github.com>
2025-09-10 14:21:16 +0300

    Password Reset with Code to email

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::post('/verifyOTP', [AuthController::class, 'verifyOTP']);
+
+Route::post('/password/forgot', [AuthController::class, 'SendResetCode']);
+Route::post('/password/reset', [AuthController::class, 'updatePassword']);
